3ef715851b155438f127b7b9cfdd1cf092253d99
Milsy18 <millarmike1@gmail.com>
2025-06-09 18:11:17 +0000

    Add market_level.py stub with compute_market_level()

---
+++ modules/market_level.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def compute_market_level(df: pd.DataFrame) -> pd.Series:
+    """
+    Placeholder stub for compute_market_level.
+    Given a DataFrame of aligned regime data, return a Series of market levels.
+    """
+    # TODO: port your Python logic here
+    return pd.Series(5, index=df.index, name="market_level")
