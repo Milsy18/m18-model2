92c7f77abb1045cc8e86c91a21e36cec1efeed21
Milsy18 <millarmike1@gmail.com>
2025-06-09 21:03:09 -0400

    Delete modules/total_score.py

--- modules/total_score.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def compute_total_score(df: pd.DataFrame) -> pd.Series:
-    """
-    Stub for total_score: returns zeros until implemented.
-    """
-    return pd.Series(0, index=df.index, name="total_score")
