a983defae424d9cc70d279c6b00250711b9264a9
Milsyl8 <millarmike1@gmail.com>
2025-06-10 00:19:36 +0000

    Fix docstring quoting in total_score stub

--- modules/total_score.py
+++ modules/total_score.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
 def compute_total_score(df: pd.DataFrame) -> pd.Series:
-    \"\"\"Stub for total_score: returns zeros until implemented.\"\"\"
+    """
+    Stub for total_score: returns zeros until implemented.
+    """
     return pd.Series(0, index=df.index, name="total_score")
